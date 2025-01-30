e0f286900e0a59d25eebb0297eaa25482d283ac6
nataraj.ramamoorthy <nataraj.ramamoorthy13@gmail.com>
2025-01-30 21:28:22 +0530

    v1

---
+++ provider.tf
@@ -0,0 +1,13 @@
+provider "google" {
+  project = "your-gcp-project-id"
+  region  = "your-gcp-region"
+}
+
+provider "aws" {
+  region = "your-aws-region"
+}
+
+provider "google" {
+  project = "your-gcp-project-id"
+  region  = "your-gcp-region"
+}
---
+++ storage_bucket.tf
@@ -0,0 +1,66 @@
+resource "google_storage_bucket" "target" {
+  name     = "your-gcs-bucket"
+  location = "US"
+}
+
+resource "google_service_account" "transfer_account" {
+  account_id   = "transfer-service-account"
+  display_name = "Transfer Service Account"
+}
+
+resource "google_project_iam_member" "storage_transfer_service" {
+  project = "your-gcp-project-id"
+  role    = "roles/storage.admin"
+  member  = "serviceAccount:${google_service_account.transfer_account.email}"
+}
+
+resource "google_storage_transfer_job" "s3_to_gcs_transfer" {
+  name        = "s3-to-gcs-transfer"
+  description = "Transfer job from S3 to GCS"
+
+  project = "your-gcp-project-id"
+
+  status = "ENABLED"
+
+  schedule {
+    schedule_start_date {
+      year  = 2025
+      month = 1
+      day   = 30
+    }
+    schedule_end_date {
+      year  = 2025
+      month = 12
+      day   = 31
+    }
+    start_time_of_day {
+      hours   = 12
+      minutes = 0
+      seconds = 0
+      nanos   = 0
+    }
+  }
+
+  transfer_spec {
+    object_conditions {
+      max_time_elapsed_since_last_modification = "600s"
+      exclude_prefixes = [
+        "requests.gz",
+      ]
+    }
+    transfer_options {
+      delete_objects_unique_in_sink = false
+    }
+    aws_s3_data_source {
+      bucket_name = var.aws_s3_bucket
+      aws_access_key {
+        access_key_id     = var.aws_access_key_id
+        secret_access_key = var.aws_secret_access_key
+      }
+    }
+    gcs_data_sink {
+      bucket_name = google_storage_bucket.s3-backup-bucket.name
+      path        = "foo/bar/"
+    }
+  }
+}
