dfa7fb1defbd211b556e7d2d6b6e80c93d6e992a
nataraj.ramamoorthy <nataraj.ramamoorthy13@gmail.com>
2025-02-01 17:45:44 +0530

    v8

--- terraform.tfvars
+++ terraform.tfvars
@@ -1,8 +1,7 @@
-config = {
-  aws_access_key_id     = "cccc"
-  aws_secret_access_key = "xxx"
-  aws_s3_bucket         = "application-gcp"
-  gcp_project_id        = "vm-group-448915"
-  gcs_bucket_name       = "application-aws"
-
-}  
+# config = {
+#   aws_access_key_id     = "cccc"
+#   aws_secret_access_key = "xxx"
+#   aws_s3_bucket         = "application-gcp"
+#   gcp_project_id        = "vm-group-448915"
+#   gcs_bucket_name       = "application-aws"
+# }
--- variable.tf
+++ variable.tf
@@ -1,10 +1,16 @@
 variable "config" {
   type = object({
-    aws_access_key_id : string
-    aws_secret_access_key : string
-    aws_s3_bucket : string
-    gcp_project_id : string
-    gcs_bucket_name : string
+    aws_access_key_id     = string
+    aws_secret_access_key = string
+    aws_s3_bucket         = string
+    gcp_project_id        = string
+    gcs_bucket_name       = string
   })
-
+  default = {
+    aws_access_key_id     = "John Doe"
+    aws_secret_access_key = "vvv"
+    aws_s3_bucket         = "true"
+    gcp_project_id        = "vm-group-448915"
+    gcs_bucket_name       = "application-aws"
+  }
 }
